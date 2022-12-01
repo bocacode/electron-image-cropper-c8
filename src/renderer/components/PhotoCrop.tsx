a0b3ab1bac9625c75f1d5400313d7848a15b8ab5
Todd Albert <todd@bocacode.com>
2022-12-01 11:41:35 -0500

    cropped image saving

--- src/renderer/components/PhotoCrop.tsx
+++ src/renderer/components/PhotoCrop.tsx
@@ -1,40 +1,50 @@
-import { useState } from "react";
-import Cropper from "react-easy-crop";
-import { readFile } from "../../main/helpers";
+import { useState, useCallback } from 'react';
+import Cropper from 'react-easy-crop';
+import { Area } from 'react-easy-crop/types';
+import { readFile, cropImageData } from '../../main/helpers';
 
 export default function PhotoCrop() {
-
   const [imageSrc, setImageSrc] = useState(null); // file data
   const [filename, setFilename] = useState(null); // file address
   const [crop, setCrop] = useState({ x: 0, y: 0 });
   const [zoom, setZoom] = useState(1);
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState<Area>();
 
-  const handleSave = () => {
-    // first save the cropped image
-    // saveCroppedImage(filename, imageSrc, croppedAreaPixels);
-    // then reset for the next photo
-    setImageSrc(null);
-    setZoom(1);
-    setCrop({ x: 0, y: 0 });
-  }
+  const handleSave = async () => {
+    // save the cropped image
+    if(croppedAreaPixels && imageSrc) {
+      // need the base64data
+      const base64data = await cropImageData(imageSrc, croppedAreaPixels);
+      const newFileName = filename + '-cropped.png';
+      window.electron.saveCroppedImage([newFileName, base64data]);
+      // then reset for the next photo
+      setImageSrc(null);
+      setZoom(1);
+      setCrop({ x: 0, y: 0 });
+    }
+  };
+
+  const onCropComplete = useCallback((croppedArea: Area, currentCroppedAreaPixels: Area) => {
+      setCroppedAreaPixels(currentCroppedAreaPixels);
+    }, []);
 
   const handleFileChange = async (e: any) => {
-    if(e.target.files && e.target.files.length) {
+    if (e.target.files && e.target.files.length) {
       // we got a file...
-      const file = e.target.files[0]
-      setFilename(file.path)
+      const file = e.target.files[0];
+      setFilename(file.path);
       // get the image data from the file...
       const imageData: any = await readFile(file);
       setImageSrc(imageData);
     }
-  }
-  if(!imageSrc) {
+  };
+  if (!imageSrc) {
     return (
       <>
         <h1>Please choose a photo to crop</h1>
         <input type="file" accept="image/*" onChange={handleFileChange} />
       </>
-    )
+    );
   }
   return (
     <>
@@ -44,8 +54,11 @@ export default function PhotoCrop() {
         zoom={zoom}
         onCropChange={setCrop}
         onZoomChange={setZoom}
-        />
-        <button onClick={handleSave} className="save-btn">Save</button>
+        onCropComplete={onCropComplete}
+      />
+      <button onClick={handleSave} className="save-btn">
+        Save
+      </button>
     </>
-  )
+  );
 }
